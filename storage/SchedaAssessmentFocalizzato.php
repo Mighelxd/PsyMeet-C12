7e5decccc81befc9e6dfdf52ede917a09041420e
panicSIMONE <simonedambrosio99@gmail.com>
2020-12-29 21:20:27 +0100

    Aggiunta scheda assessment focalizzato e generalizzato

---
+++ storage/SchedaAssessmentFocalizzato.php
@@ -0,0 +1,109 @@
+<?php
+/*
+ * Gestione Sedute
+ * Questa classe contiene le informazioni relative all'oggetto scheda assessment focalizzato
+ * Autore: Simone D'Ambrosio
+ * Versione: 0.1
+ * 2020 Copyright by PsyMeet - University of Salerno
+ */
+class SchedaAssessmentFocalizzato
+{
+    private $id_scheda;
+    private $data;
+    private $analisi_fun;
+    private $m_a;
+    private $m_b;
+    private $m_c;
+    private $appunti;
+    private $n_episodi;
+    private $id_terapia;
+    private static $table_name="SchedaAssessmentFocalizzato";
+    
+    public function __construct($id_scheda, $data, $analisi_fun, $m_a, $m_b,$m_c, $appunti, $n_episodi, $id_terapia)
+    {
+        $this->id_scheda = $id_scheda;
+        $this->data = $data;
+        $this->analisi_fun= $analisi_fun;
+        $this->m_a = $m_a;
+        $this->m_b = $m_b;
+        $this->m_c =$m_c;
+        $this->appunti =$appunti;
+        $this->n_episodi =$n_episodi;
+        $this->id_terapia = $id_terapia;
+    }
+    public function getIdScheda()
+    {
+        return $this -> id_scheda;
+    }
+    public function getData()
+    {
+        return $this -> data;
+    }
+    public function getAnalisiFun()
+    {
+        return $this -> analisi_fun;
+    }
+    public function getMA()
+    {
+        return $this -> m_a;
+    }
+    public function getMB()
+    {
+        return $this -> m_b;
+    } public function getMC()
+    {
+        return $this -> m_c;
+    }
+    public function getAppunti()
+    {
+        return $this -> appunti;
+    }
+    public function getNEpisodi()
+    {
+        return $this -> n_episodi;
+    }
+    public function getIdTerapia()
+    {
+        return $this -> id_terapia;
+    }
+    public function getArray(){
+        return array("id_scheda" => $this->id_scheda, "data" => $this->data, "an_fun" => $this->analisi_fun, "m_a" => $this->m_a, "m_b" => $this->m_b, "m_c" => $this->m_c, "appunti" => $this->appunti, "n_ep" => $this->n_episodi, "id_terapia" => $this->id_terapia);
+    }
+    public function setIdScheda($id_scheda)
+    {
+        return $this -> id_scheda= $id_scheda;
+    }
+    public function setData($data)
+    {
+        return $this -> data = $data;
+    }
+    public function setAnalisiFun($analisi_fun)
+    {
+        return $this -> analisi_fun = $analisi_fun;
+    }
+    public function setMA($m_a)
+    {
+        return $this -> m_a = $m_a;
+    }
+    public function setMB($m_b)
+    {
+        return $this -> m_b = $m_b;
+    }
+    public function setMC($m_c)
+    {
+        return $this -> m_c = $m_c;
+    }
+    public function setAppunti($appunti)
+    {
+        return $this -> appunti = $appunti;
+    }
+    public function setNEpisodi($n_episodi)
+    {
+        return $this -> n_episodi = $n_episodi;
+    }
+    public function setIdTerapia($id_terapia)
+    {
+        return $this -> id_terapia = $id_terapia;
+    }
+}
+
---
+++ storage/SchedaAssessmentGeneralizzato.php
@@ -0,0 +1,130 @@
+<?php
+/*
+ * Gestione Sedute
+ * Questa classe contiene le informazioni relative all'oggetto scheda assessment generalizzato
+ * Autore: Simone D'Ambrosio
+ * Versione: 0.1
+ * 2020 Copyright by PsyMeet - University of Salerno
+ */
+class SchedaAssessmentFocalizzato
+{
+    private $id_scheda;
+    private $data;
+    private $autoreg_positivi;
+    private $autoreg_negativi;
+    private $cognitive_positivi;
+    private $cognitive_negativi;
+    private $self_management_negativi;
+    private $sociali_positivi;
+    private $sociali_negativi;
+    private $self_management_positivi;
+    private $id_terapia;
+    private static $table_name="SchedaAssessmentGeneralizzato";
+    
+    public function __construct($id_scheda, $data, $autoreg_positivi, $autoreg_negativi, $cognitive_positivi, $cognitive_negativi, $self_management_negativi, $sociali_positivi, $sociali_negativi,$self_management_positivi, $id_terapia)
+    {
+        $this->id_scheda = $id_scheda;
+        $this->data = $data;
+        $this->autoreg_positivi= $autoreg_positivi;
+        $this->autoreg_negativi = $autoreg_negativi;
+        $this->cognitive_positivi = $cognitive_positivi;
+        $this->cognitive_negativi =$cognitive_negativi;
+        $this->self_management_negativi =$self_management_negativi;
+        $this->sociali_positivi =$sociali_positivi;
+        $this->sociali_negativi =$sociali_negativi;
+        $this->self_management_positivi =$self_management_positivi;
+        $this->id_terapia = $id_terapia;
+    }
+    public function getIdScheda()
+    {
+        return $this -> id_scheda;
+    }
+    public function getData()
+    {
+        return $this -> data;
+    }
+    public function getAutoregPositivi()
+    {
+        return $this -> autoreg_positivi;
+    }
+    public function getAutoregNegativi()
+    {
+        return $this -> autoreg_negativi;
+    }
+    public function getCognitivePositivi()
+    {
+        return $this -> cognitive_positivi;
+    }
+    public function getCognitiveNegativi()
+    {
+        return $this -> cognitive_negativi;
+    }
+    public function getSelfManagementNegativi()
+    {
+        return $this -> self_management_negativi;
+    } 
+    public function getSocialiPositivi()
+    {
+        return $this -> sociali_positivi;
+    }
+    public function getSocialiNegativi()
+    {
+        return $this -> sociali_negativi;
+    }
+    public function getSelfManagementPositivi()
+    {
+        return $this -> self_management_positivi;
+    } 
+    public function getIdTerapia()
+    {
+        return $this -> id_terapia;
+    }
+    public function getArray(){
+        return array("id_scheda" => $this->id_scheda, "data" => $this->data, "au_pos" => $this->autoreg_positivi, "au_neg" => $this->autoreg_negativi, "co_pos" => $this->cognitive_positivi, "co_neg" => $this->cognitive_negativi, "se_pos" => $this->self_management_positivi, "so_pos" => $this->sociali_positivi, "so_neg" => $this->sociali_negativi, "se_neg" => $this->self_management_negativi, "id_terapia" => $this->id_terapia);
+    }
+    public function setIdScheda($id_scheda)
+    {
+        return $this -> id_scheda= $id_scheda;
+    }
+    public function setData($data)
+    {
+        return $this -> data = $data;
+    }
+    public function setAutoregPositivi($autoreg_positivi)
+    {
+        return $this -> autoreg_positivi = $autoreg_positivi;
+    }
+    public function setAutoregNegativi($autoreg_negativi)
+    {
+        return $this -> autoreg_negativi = $autoreg_negativi;
+    }
+    public function setCognitivePositivi($cognitive_positivi)
+    {
+        return $this -> cognitive_positivi = $cognitive_positivi;
+    }
+    public function setCognitiveNegativi($cognitive_negativi)
+    {
+        return $this -> cognitive_negativi = $cognitive_negativi;
+    }
+    public function setSelfManagementNegativi($self_management_negativi)
+    {
+        return $this -> self_management_negativi = $self_management_negativi;
+    }
+    public function setSocialiPositivi($sociali_positivi)
+    {
+        return $this -> sociali_positivi = $nsociali_positivi;
+    }
+    public function setSocialiNegativi($sociali_negativi)
+    {
+        return $this -> sociali_negativi = $nsociali_negativi;
+    }
+    public function setSelfManagementPositivi($self_management_positivi)
+    {
+        return $this -> self_management_positivi = $self_management_positivi;
+    }
+    public function setIdTerapia($id_terapia)
+    {
+        return $this -> id_terapia = $id_terapia;
+    }
+}
+
