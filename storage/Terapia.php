a7328e8cfaec6ab63984e19018a5b00e1d2031e6
MartinoDA11 <76061096+MartinoDA11@users.noreply.github.com>
2020-12-29 19:26:40 +0100

    creazione classe Terapia

---
+++ storage/Terapia.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * Gestione Terapia
+ * Questa classe contiene le informazioni relative all'oggetto terapia
+ * Autore: Martino D'Auria
+ * Versione: 0.1
+ * 2020 Copyright by PsyMeet - University of Salerno
+ */
+class Terapia
+{
+    private $id_terapia;
+    private $data;
+    private $descrizione;
+    private $cf_prof;
+    private $cf;
+    private static $nome_tabella="terapia";
+    
+    public function __construct($id_terapia, $data,$descrizione, $cf_prof, $cf)
+    {
+        $this->id_terapia = $id_terapia;
+        $this->data = $data;
+        $this->descrizione= $descrizione;
+        $this->cf_prof = $cf_prof;
+        $this->cf = $cf;
+        
+    }
+    public function getIdTerapia()
+    {
+        return $this -> id_terapia;
+    }
+    public function getData()
+    {
+        return $this -> data;
+    }
+    public function getDescrizione()
+    {
+        return $this -> descrizione;
+    }
+    public function getCfProf()
+    {
+        return $this -> cf_prof;
+    }
+    public function getCf()
+    {
+        return $this -> cf;
+    }
+    public function getArray(){
+        return array("id_terapia" => $this->id_terapia, "data" => $this->data, "descr" => $this->descrizione, "cf_prof" => $this->cf_prof, "cf" => $this->cf);
+    }
+    public function setIdTerapia($id_terapia)
+    {
+        return $this -> id_terapia= $id_terapia;
+    }
+    public function setData($data)
+    {
+        return $this -> data = $data;
+    }
+    public function setDescrizione($descrizione)
+    {
+        return $this -> descrizione = $descrizione;
+    }
+    public function setCf_Prof($cf_prof)
+    {
+        return $this -> cf_prof = $cf_prof;
+    }
+    public function setCf($cf)
+    {
+        return $this -> cf = $cf;
+    }
+}
