97237839108a5e3eed8850b69aafee2d8b0457f2
Peppe8765 <72818194+Peppe8765@users.noreply.github.com>
2020-12-31 00:05:25 +0100

    aggiunta funzione getArrey() per Paziente.php e testato alcune funzioni con il database

--- storage/test.php
+++ storage/test.php
@@ -1,7 +1,11 @@
 <?php
-    include("DatabaseInterface.php"); 
+    include("DatabaseInterface.php");
     include "CartellaClinica.php";
     include "Appuntamento.php";
+    include "Paziente.php";
+
+
+
 
     $arr = array("id_cartella_clinica" => 1,);
     $result = DatabaseInterface::selectQueryById($arr,"cartellaclinica");
@@ -9,9 +13,30 @@
     $cc = new CartellaClinica($arr[0],$arr[1],$arr[2],$arr[3],$arr[4],$arr[5],$arr[6]);
     $cc -> setFarmaci("test");
     $result = DatabaseInterface::updateQueryById($cc->getArray(),CartellaClinica::$table_name);
+    echo FunArray::array_key_first($cc->getArray());
+    echo $arr[FunArray::array_key_first($cc->getArray())];
     var_dump($result);
-    /*$app = array("id_appuntamento" => 1,);
-    $res = DatabaseInterface::selectQueryById($arr,"appuntamento");
-    $app = $res->fetch_array();*/
 
+/*  $app = array("id_appuntamento" => 1,);
+    $res = DatabaseInterface::selectQueryById($arr,"appuntamento");
+    $app = $res->fetch_array();
+*/
+
+
+// test classe Paziente.php Ferrante Giuseppe (lo scrivo giusto per ricordarmi che questo è il mio test)
+/*
+$arr = array("cf" => '"NSTFNC94M23H703G"',);
+echo FunArray::array_key_first($arr);
+echo $arr[FunArray::array_key_first($arr)];
+$result = DatabaseInterface::selectQueryById($arr,"paziente");
+if($result == false)
+  echo "non va";
+$arr = $result -> fetch_array();
+  $paz = new Paziente($arr[0], $arr[1], $arr[2], $arr[3], $arr[4], $arr[5], $arr[6], $arr[7], $arr[8], $arr[9], $arr[10]);
+  $paz -> setNome("provaNome");
+  $result = DatabaseInterface::updateQueryById($paz->getArray(),"paziente");
+  echo FunArray::array_key_first($paz->getArray());
+  echo $arr[FunArray::array_key_first($paz->getArray())];
+  var_dump($result);
+*/
 ?>
