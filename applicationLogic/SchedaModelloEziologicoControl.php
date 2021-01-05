6c6de3d2db4d01011dd4ecc8535a6426456115f8
FrancescoC579 <76061336+FrancescoC579@users.noreply.github.com>
2021-01-06 00:44:37 +0100

    control per modello eziologico

---
+++ applicationLogic/SchedaModelloEziologicoControl.php
@@ -0,0 +1,25 @@
+?php
+/*
+* SchedaModelloEziologicoControl
+* Questa Control fornisce tutti i metodi relativi alla scheda modello eziologico
+* Versione: 0.1
+* 2020 Copyright by PsyMeet - University of Salerno
+*/
+    include "../storage/DatabaseInterface.php ";
+    include "../storage/SchedaModelloEziologico.php";
+    if(!$_SERVER["REQUEST_METHOD"] == "POST"){
+        header("Location: ../interface/Professionista/SchedaModelloEziologico.html");
+        exit;
+    }
+    else{
+            $id_scheda=$_POST["id_scheda"];
+            $data=$_POST["data"];
+            $fattori_causativi=$_POST["fattori_causativi"];
+            $fattori_precipitanti=$_POST["fattori_precipitanti"]);
+            $fattori_mantenimento=$_POST["fattori_mantenimento"];
+            $relazione_finale=$_POST["relazione_finale"];
+            $id_terapia=$_POST["id_terapia"];
+            $schedamodelloeziologico = new SchedaModelloEziologico($id_scheda,$data,$fattori_causativi,$fattori_precipitanti,$fattori_mantenimento,$relazione_finale,$id_terapia);
+            $result = DatabaseInterface::insertQuery($professionista->getArray(),$professionista->tableName);
+        }
+?>
