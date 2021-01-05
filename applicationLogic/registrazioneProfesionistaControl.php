a23f9b1bc628f22160da2df66748610a917d4d36
Mighelxd <mikyss38@gmail.com>
2021-01-05 18:46:46 +0100

    Aggiunta classe Professionista, cominciata control registrazione e modifiche alla view della registrazione.

---
+++ applicationLogic/registrazioneProfesionistaControl.php
@@ -0,0 +1,28 @@
+<?php
+    include "../storage/DatabaseInterface.php ";
+    include "../storage/professionista.php";
+    if(!$_SERVER["REQUEST_METHOD"] == "POST"){
+        header("Location: psymeet/interface/Professionista/registrazioneProfessionista.html");
+        exit;
+    }
+    else{
+            $nome=$_POST["nome"];
+            $cognome=$_POST["cognome"];
+            $data_nascita=$_POST["dataN"];
+            $codice_fiscale=$_POST["cf"];
+            $titolo_studio=$_POST["titoloStudio"];
+            $pubblicazioni=$_POST["pubblicazioni"];
+            $esperienze=$_POST["esperienze"];
+            $indirizzo_studio=$_POST["indirizzoStudio"];
+            $telefono=$_POST["telefono"];
+            $cellulare=$_POST["cellulare"];
+            $p_iva=$_POST["pIva"];
+            $polizza_rc=$_POST["polizzaRc"];
+            $n_iscrizione_albo=$_POST["nIscrizioneAlbo"];
+            $email=$_POST["email"];
+            $password=$_POST["password"];
+            $conferma_password=$_POST["confermaPassword"];
+            $videoProfessionista=$_POST["videoPresentazione"];
+            $professionista = new Professionista($codice_fiscale,$nome,$cognome,$data_nascita,$email,$telefono,$cellulare,$password,$indirizzo_studio,$esperienze,$pubblicazioni,$titolo_studio,$n_iscrizione_albo,$p_iva,NULL,NULL,$polizza_rc,$videoProfessionista,NULL);
+        }
+?>
---
+++ storage/Professionista.php
@@ -0,0 +1,194 @@
+<?php
+    class Professionista{
+        private $cfProf;
+        private $nome;
+        private $cognome;
+        private $dataNascita;
+        private $email;
+        private $telefono;
+        private $cellulare;
+        private $password;
+        private $indirizzoStudio;
+        private $esperienze;
+        private $pubblicazioni;
+        private $titoloStudio;
+        private $nIscrizioneAlbo;
+        private $partitaIva;
+        private $pec;
+        private $specializzazione;
+        private $polizzaRc;
+        private $immagineProfessionista;
+        private $videoProfessionista;
+
+        public function __construct($cf,$n,$c,$date,$e,$tel,$cell,$pass,$indiSt,$esp,$pub,$titSt,$nIsc,$pIva,$p,$spec,$polRc,$video,$img){
+            $this->cfProf=$cf;
+            $this->nome=$n;
+            $this->cognome=$c;
+            $this->dataNascita=$date;
+            $this->email=$e;
+            $this->telefono=$tel;
+            $this->cellulare=$cell;
+            $this->password=$pass;
+            $this->$indirizzoStudio=$indiSt;
+            $this->esperienze=$esp;
+            $this->pubblicazioni=$pub;
+            $this->titoloStudio=$titSt;
+            $this->nIscrizioneAlbo=$nIsc;
+            $this->partitaIva=$pIva;
+            $this->pec=$p;
+            $this->specializzazione=$spec;
+            $this->polizzaRc=$polRc;
+            $this->videoProfessionista=$video;
+            $this->immagineProfessionista=$img;
+        }
+
+        public function setNome($n){
+            $this->nome=$n;
+        }
+
+        public function setCognome($c){
+            $this->cognome=$c;
+        }
+
+        public function setDataNascita($d){
+            $this->dataNascita=$d;
+        }
+
+        public function setEsperienze($esp){
+            $this->esperienze=$esp;
+        }
+
+        public function setTelefono($tel){
+            $this->telefono=$tel;
+        }
+
+        public function setCellulare($cell){
+            $this->cellulare=$cell;
+        }
+
+        public function setPassword($pass){
+            $this->password=$pass;
+        }
+
+        public function setIndirizzoStudio($indiSt){
+            $this->indirizzoStudio=$indiSt;
+        }
+
+        public function setEmail($e){
+            $this->email=$e;
+        }
+
+        public function setPubblicazione($pub){
+            $this->pubblicazione=$pub;
+        }
+
+        public function setTitoloStudio($titSt){
+            $this->titoloStudio=$titSt;
+        }
+
+        public function setNIscrizioneAlbo($nIsc){
+            $this->nIscrizioneAlbo=$nIsc;
+        }
+
+        public function setPartitaIva($pIva){
+            $this->partitaIva=$pIva;
+        }
+
+        public function setPec($p){
+            $this->pec=$p;
+        }
+
+        public function setSpecializzazione($spec){
+            $this->spec;
+        }
+
+        public function setPolizzaRc($polRc){
+            $this->polizzaRc=$polRc;
+        }
+
+        public function setVideoProfessionista($video){
+            $this->videoProfessionista=$video;
+        }
+
+        public function setImmagineProfessionista($img){
+            $this->immagineProfessionista;
+        }
+
+        public function getNome(){
+            return $this->nome;
+        }
+
+        public function getCognome(){
+            return $this->cognome;
+        }
+
+        public function getDataNascita(){
+            return $this->dataNascita;
+        }
+
+        public function getEsperienze(){
+            return $this->esperienze;
+        }
+
+        public function getTelefono(){
+            return $this->telefono;
+        }
+
+        public function getCellulare(){
+            return $this->cellulare;
+        }
+
+        public function getPassword(){
+            return $this->password;
+        }
+
+        public function getIndirizzoStudio(){
+            return $this->indirizzoStudio;
+        }
+
+        public function getEmail(){
+            return $this->email;
+        }
+
+        public function getPubblicazione(){
+            return $this->pubblicazione;
+        }
+
+        public function getTitoloStudio(){
+            return $this->titoloStudio;
+        }
+
+        public function getNIscrizioneAlbo(){
+            return $this->nIscrizioneAlbo;
+        }
+
+        public function getPartitaIva(){
+            return $this->partitaIva;
+        }
+
+        public function getPec(){
+            return $this->pec;
+        }
+
+        public function getSpecializzazione(){
+            return $this->specializzazione;
+        }
+
+        public function getPolizzaRc(){
+            return $this->polizzaRc;
+        }
+
+        public function getVideoPresentazione(){
+            return $this->videoPresentazione;
+        }
+
+        public function getImmagineProfessionista(){
+            return $this->immagineProfessionista;
+        }
+
+
+        public function getArray(){
+            return array("cfProf" => $this->cfProf, "nome" => $this->nome, "cognome" => $this->cognome, "dataNascita" => $this->dataNascita, "email" => $this->email, "telefono" => $this->telefono, "cellulare" => $this->cellulare, "passwor"=> $this->password, "indirizzoStudio"=> $this->indirizzoStudio, "esperienze" => $this->esperienze, "pubblicazioni" => $this->pubblicazioni, "titoloStudio" => $this->titoloStudio, "nIscrizioneAlbo" => $this->nIscrizioneAlbo, "partitaIva" => $this->partitaIva, "pec" => $this->partitaIva, "specializzazione" => $this->specializzazione, "polizzaRc" => $this->$polizzaRc, "videoProfessionista" => $this->videoProfessionista, "foto_profilo_professionista" => $this->image);
+        }
+    }
+?>
