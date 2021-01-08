ce4c54a4b413faf558f9f52dc3f9c75cb84fadc5
Mighelxd <mikyss38@gmail.com>
2021-01-08 18:57:35 +0100

    Modifiche html

--- interface/Professionista/registrazioneProfessionista.php
+++ interface/Professionista/registrazioneProfessionista.php
@@ -35,7 +35,7 @@
 <body class="hold-transition register-page">
 <div class="register-box">
   <div class="register-logo">
-    <a href="../../index2.html"><b>Psy</b>Meet</a>  			<!-- ci va il link alla homepage -->
+    <a href="../Utente/Homepage.php"><b>Psy</b>Meet</a>  			<!-- ci va il link alla homepage -->
   </div>
 
   <div class="card">
