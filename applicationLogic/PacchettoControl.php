a7c262ad7d9803e7086b02384ee4ee534587f593
TherealBarnar <giuseppestandavino@gmail.com>
2021-01-11 16:47:55 +0100

    Modifiche a pacchetto control

--- applicationLogic/PacchettoControl.php
+++ applicationLogic/PacchettoControl.php
@@ -1,12 +1,12 @@
 <?php
-include('../../storage/Pacchetto.php');
+
 define("TABLE_NAME","pacchetto");
 
 
 class PacchettoControl{
 
   static function selectAllPacchetto(){
-    //Questa action cerca di recuperare tutti i pacchetti
+    //Questa action cerca di recuperare tutti i pacchetti che vi sono nel DB
     $id = 1;
     $arrKey = array("id_pacchetto"=>$id);
     $allPac = DatabaseInterface::selectQueryByAtt($arrKey,TABLE_NAME);
@@ -15,6 +15,16 @@ class PacchettoControl{
       $arr[]=$pac;
     }
     return $arr;
+  }
+
+  //metodo che recupera tutti i pacchetti per un dato Professionista bisogna
+  // avere un reference verso la relazione Pacchetto del DB magari tramite il Cf del
+  //professionista cosi da poter associare ad ogni professionista i pacchetti che sono
+  //registrati nel DB
+  // oppure aggiungere una vista
+
+  static function selectAllPacchettoProf($cfProfessionista){
+    $arrpac= array("")
   }
 }
  ?>
