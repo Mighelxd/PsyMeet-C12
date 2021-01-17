dee30fc7931d96e3360b857237b60aed45eb91cf
marcocampione683 <marko.93_@hotmail.it>
2021-01-17 18:14:44 +0100

    Aggiunto eccezioni follow up in storage completate eccezioni in storage

--- storage/SchedaFollowUp.php
+++ storage/SchedaFollowUp.php
@@ -8,26 +8,40 @@
  */
 class SchedaFollowUp
 {
-    private $id_scheda;
+    private $idScheda;
     private $data;
     private $ricadute;
-    private $esiti_positivi;
-    private $id_terapia;
+    private $esitiPositivi;
+    private $idTerapia;
     private $tipo;
     public static $tableName="schedafollowup";
 
-    public function __construct($id_scheda, $data,$ricadute, $esiti_positivi, $id_terapia,$tipo)
+    public function __construct($idScheda, $data,$ricadute, $esitiPositivi, $idTerapia,$tipo)
     {
-        $this->id_scheda = $id_scheda;
+        date_default_timezone_set("Europe/Rome");
+        $currDate = date("Y-m-d");
+        if($data == null || $ricadute == null || $esitiPositivi == null || $esitiPositivi == null || $idTerapia == null || $tipo == null){
+            throw new Exception("Alcuni valori non definiti!");
+        }
+        else if($data<$currDate){
+            throw new Exception("Data non disponibile!");
+        }
+        else if($idTerapia<1){
+            throw new Exception("Id terapia non valida!");
+        }
+        else if($tipo != "Scheda Follow Up"){
+            throw new Exception("Tipo scheda non compatibile!");
+        }
+        $this->idScheda = $idScheda;
         $this->data = $data;
         $this->ricadute= $ricadute;
-        $this->esiti_positivi = $esiti_positivi;
-        $this->id_terapia = $id_terapia;
+        $this->esitiPositivi = $esitiPositivi;
+        $this->idTerapia = $idTerapia;
         $this->tipo = $tipo;
     }
     public function getIdScheda()
     {
-        return $this -> id_scheda;
+        return $this -> idScheda;
     }
     public function getData()
     {
@@ -39,41 +53,58 @@ class SchedaFollowUp
     }
     public function getEsitiPositivi()
     {
-        return $this -> esiti_positivi;
+        return $this -> esitiPositivi;
     }
     public function getIdTerapia()
     {
-        return $this -> id_terapia;
+        return $this -> idTerapia;
     }
     public function getTipo()
     {
         return $this -> tipo;
     }
     public function getArray(){
-        return array("id_scheda" => $this->id_scheda, "data" => $this->data, "ricadute" => $this->ricadute, "esiti_positivi" => $this->esiti_positivi, "id_terapia" => $this->id_terapia,"tipo" => $this->tipo);
+        return array("id_scheda" => $this->idScheda, "data" => $this->data, "ricadute" => $this->ricadute, "esiti_positivi" => $this->esitiPositivi, "id_terapia" => $this->idTerapia,"tipo" => $this->tipo);
     }
-    public function setIdScheda($id_scheda)
+    public function setIdScheda($idScheda)
     {
-         $this -> id_scheda= $id_scheda;
+         $this -> idScheda= $idScheda;
     }
     public function setData($data)
     {
+        date_default_timezone_set("Europe/Rome");
+        $currDate = date("Y-m-d");
+        if($data == null || $data<$currDate){
+            throw new Exception("Nuova data non disponibile!");
+        }
          $this -> data = $data;
     }
     public function setRicadute($ricadute)
     {
+        if($ricadute == null){
+            throw new Exception("Nuovo campo ricadute non valido!");
+        }
          $this -> ricadute = $ricadute;
     }
-    public function setEsitiPositivi($esiti_positivi)
+    public function setEsitiPositivi($esitiPositivi)
     {
-         $this -> esiti_positivi = $esiti_positivi;
+        if($esitiPositivi == null){
+            throw new Exception("Nuovo campo esiti positivi non valido!");
+        }
+         $this -> esitiPositivi = $esitiPositivi;
     }
-    public function setIdTerapia($id_terapia)
+    public function setIdTerapia($idTerapia)
     {
-         $this -> id_terapia = $id_terapia;
+        if($idTerapia == null || $idTerapia<1){
+            throw new Exception("Nuovo id terapia non valido!");
+        }
+         $this -> idTerapia = $idTerapia;
     }
     public function setTipo($tipo)
     {
+        if($tipo == null || $tipo!="Scheda Follow Up"){
+            throw new Exception("Nuovo tipo non compatibile!");
+        }
          $this -> tipo =$tipo;
     }
 }
