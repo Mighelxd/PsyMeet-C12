1370e3e08320b3047dea058741e838c6b3ce7401
Mighelxd <mikyss38@gmail.com>
2021-01-18 22:19:51 +0100

    Modifiche front end

--- interface/Professionista/cartellaClinica.php
+++ interface/Professionista/cartellaClinica.php
@@ -83,18 +83,18 @@
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
     <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-        <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <a href="#" class="nav-link">Home</a>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <a href="#" class="nav-link">Contact</a>
-        </li>
-    </ul>
+        <!-- Left navbar links -->
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+            </li>
+            <li class="nav-item d-none d-sm-inline-block">
+                <a href="#" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item d-none d-sm-inline-block">
+                <a href="#" class="nav-link">Contact</a>
+            </li>
+        </ul>
 <!-- MENU' A SINISTRA -->
 
   <!-- Main Sidebar Container -->
@@ -249,7 +249,7 @@
     <!-- /.sidebar -->
   </aside>
 <!-- FINE MENU' A SINISTRA -->
-
+</div>
 
 
 
