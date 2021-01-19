3d54c53deae21b6c826a48407114a72a827fd13b
Mighelxd <mikyss38@gmail.com>
2021-01-20 00:10:06 +0100

    Cambiamenti test

--- test/CartellaClinicaControlTest.php
+++ test/CartellaClinicaControlTest.php
@@ -62,4 +62,22 @@ class CartellaClinicaControlTest extends TestCase
         self::assertEquals(true,$response["esito"]);
         curl_close($curl);
     }
+    public function testModificaCartellaClinicaFail1(){
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'http://localhost/psymeet-c12/applicationLogic/gestioneCartellaClinica.php',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_COOKIE=> session_name()."="."sessione;"."codiceFiscale=RSSMRC80R12H703U;"."tipo=professionista;",
+            CURLOPT_POSTFIELDS => array('umo' => 'ciao','relaz'=>"5",'patol'=>"ciao",'farma'=>"ciao",'azione' => 'modifica','codFiscalePaz' => 'NSTFNC94M23H703G'),
+        ));
+        $response = curl_exec($curl);
+        self::assertStringContainsStringIgnoringCase("Valore qualit. umore non corretto!",$response);
+        curl_close($curl);
+    }
 }
