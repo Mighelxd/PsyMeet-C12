8caeb28e0b862a49a04606c73b254e1a9a002a0b
FrancescoC579 <76061336+FrancescoC579@users.noreply.github.com>
2021-01-20 18:53:34 +0100

    test follow up

---
+++ test/SchedaFollowUpTest.php
@@ -0,0 +1,39 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+include 'storage/SchedaFollowUp.php';
+
+class SchedaFollowUpTest extends TestCase
+{
+
+    public function testSetRicadute()
+    {
+        $schedafollowup= new SchedaFollowUp(1, "2021-01-20", "aaaa", "bbbb", "1", "Scheda Follow Up");
+        $schedafollowup -> setRicadute("ricadute updated");
+        self::assertEquals("ricadute updated", $schedafollowup->getRicadute());
+    }
+
+    public function testGetRicadute()
+    {
+        {
+            $schedafollowup = new SchedaFollowUp(1, "2021-01-20", "aaaa", "aaaa", "1", "Scheda Follow Up");
+            self::assertEquals("aaaa",$schedafollowup->getRicadute());
+
+        }
+    }
+
+    public function testSetEsitiPositivi()
+    {
+        $schedafollowup= new SchedaFollowUp(1, "2021-01-20", "aaaa", "bbbb", "1", "Scheda Follow Up");
+        $schedafollowup -> setEsitiPositivi("esiti positivi updated");
+        self::assertEquals("esiti positivi updated", $schedafollowup->getEsitiPositivi());
+    }
+
+    public function testGetEsitiPositivi()
+    {
+        $schedafollowup = new SchedaFollowUp(1, "2021-01-20", "aaaa", "bbbb", "1", "Scheda Follow Up");
+        self::assertEquals("bbb",$schedafollowup->getEsitiPositivi());
+    }
+}
