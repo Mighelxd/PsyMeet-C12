b9562cb6c5e7585c9e4cf9a72c76572239bb69b6
MartinoDA11 <76061096+MartinoDA11@users.noreply.github.com>
2021-01-29 17:41:21 +0100

    Correzione reindirizzamento registrazione.

--- applicationLogic/AreaInformativaControl.php
+++ applicationLogic/AreaInformativaControl.php
@@ -99,7 +99,6 @@ class AreaInformativaControl
 			$prof = new Professionista($arr[0], $arr[1], $arr[2], $arr[3], $arr[4], $arr[5], $arr[6], $arr[7], $arr[8], $arr[9], $arr[10], $arr[11], $arr[12], $arr[13], $arr[14], $arr[15], $arr[16], $arr[17]);
 			return $prof;
 		} catch (Exception $e) {
-			echo $e->getMessage();
 			$_SESSION['eccareaprof'] = $e->getMessage();
 			return null;
 		}
