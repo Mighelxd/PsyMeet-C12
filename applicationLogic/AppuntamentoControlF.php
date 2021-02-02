30420e327fab7c08288713b6293d75e9439701cd
marcocampione683 <marko.93_@hotmail.it>
2021-02-02 20:12:51 +0100

    Aggiunti commenti ad appuntamento ed appuntamento control

--- applicationLogic/AppuntamentoControlF.php
+++ applicationLogic/AppuntamentoControlF.php
@@ -1,8 +1,22 @@
 <?php
-
-
+/*
+	* AppuntamentoControl
+	* Questo control fornisce tutte le funzioni che si possono fare per Appuntamento
+	* Autore: Marco Campione
+	* Versione: 1.0
+	* 2020 Copyright by PsyMeet - University of Salerno
+*/
 class AppuntamentoControlF
 {
+    /*
+	* addApp
+	* data, ora, descrizione, codice fiscale professionista, codice fiscale paziente
+    * Aggiunge un nuovo appuntamento
+    * La funzione restituisce true in caso di successo, un'eccezione altrimenti
+	* Autore: Marco Campione
+	* Versione: 1.0
+	* 2020 Copyright by PsyMeet - University of Salerno
+*/
 	public static function addApp($data, $ora, $desc, $cfProf, $cf)
 	{
 		try {
@@ -18,8 +32,15 @@ class AppuntamentoControlF
 			return $e->getMessage();
 		}
 	}
-
-
+    /*
+        * modApp
+        * id, data, ora, descrizione, codice fiscale professionista, codice fiscale paziente, data prima della modifica, ora prima della modifica, descrizione prima modifica, codice fiscale paziente prima modifica
+        * Modifica un appuntamento
+        * La funzione restituisce true in caso di successo, un'eccezione altrimenti
+        * Autore: Marco Campione
+        * Versione: 1.0
+        * 2020 Copyright by PsyMeet - University of Salerno
+    */
 	public static function modApp($id, $data, $ora, $desc, $cfProf, $cf, $oldD, $oldO, $oldDe, $oldCfPaz)
 	{
 		try {
